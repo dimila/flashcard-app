dc0ade78a5d51c9398c8d7054662784216ec039b
dimila <dimamilayev@Dimas-MacBook-Pro.local>
2019-09-20 15:35:10 +0200

    flashcard-setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Navigation from './Navigation'
+import HomePage from './HomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const [cards, setCards] = useState([
+        {
+            title: 'Foo',
+            question: 'What?',
+            answer: 'That!',
+        },
+        {
+            title: 'Bar',
+            question: 'This?',
+            answer: 'That!',
+        },
+    ])
+
+    function renderPage() {
+        const pages = {
+            0: <HomePage cards={cards} />,
+            1: <section>Practice</section>,
+            2: <section>Bookmarks</section>,
+            3: <section>Settings</section>,
+        }
+
+        return pages[activeIndex] || <section>404</section>
+    }
+
+    return (
+        <div className="App">
+            {renderPage()}
+            <Navigation
+                buttonTexts={['Home', 'Practice', 'Bookmarks', 'Settings']}
+                onClick={setActiveIndex}
+            />
+        </div>
+    )
 }
-
-export default App;
